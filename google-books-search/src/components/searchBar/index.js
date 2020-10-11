2c519f101c459cdc1df9dbd57bb34953728ba24b
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-11 23:29:13 +1100

    SearchBar component done

--- google-books-search/src/components/searchBar/index.js
+++ google-books-search/src/components/searchBar/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const SearchBar = () => {
-  return (
-    <div className="d-flex justify-content-center">
-      <Jumbotron className="container-fluid  m-5 clearfix" style={{ padding: '.5rem' }}>
-        <h1 className="d-flex justify-content-center display-4">SearchBar</h1>
-        <p className="d-flex justify-content-center lead">Search and Save Books of Interest</p>
-      </Jumbotron>
-    </div>
-  );
+    return (
+        <div className="d-flex justify-content-center">
+            <Jumbotron className="container-fluid  m-5 clearfix" style={{ padding: '.5rem', backgroundColor: "#F8F9FA" }}>
+            <p className="lead">Book Search</p>
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleEmail">Book</Label>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="Book Title" />
+                    </FormGroup>
+                    <Button className="float-right">Search</Button>
+                </Form>
+            </Jumbotron>
+        </div>
+    );
 };
 
 export default SearchBar;
--- google-books-search/src/components/titleHeader/index.js
+++ google-books-search/src/components/titleHeader/index.js
@@ -4,7 +4,7 @@ import { Jumbotron, Button } from 'reactstrap';
 const TitleHeader = () => {
   return (
     <div className="d-flex justify-content-center">
-      <Jumbotron className="container-fluid  m-5 clearfix" style={{ padding: '.5rem' }}>
+      <Jumbotron className="container-fluid  m-5 clearfix" style={{ padding: '.5rem',backgroundColor: "#F8F9FA" }}>
         <h1 className="d-flex justify-content-center display-4">(REACT) Google Books Search</h1>
         <p className="d-flex justify-content-center lead">Search and Save Books of Interest</p>
       </Jumbotron>
