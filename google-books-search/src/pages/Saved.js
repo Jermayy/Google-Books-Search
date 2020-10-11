4702ab6a23bd6084ced044788d623bf9193a9750
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-11 22:33:42 +1100

    Initial pages structure done

---
+++ google-books-search/src/pages/Saved.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Saved = (props) => {
+  return (
+    <div className="d-flex justify-content-center">
+      <p>HELLO Saved</p>
+    </div>
+  );
+};
+
+export default Saved;
