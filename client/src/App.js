283b260de25d4b4ec9d693c1401f922f0e87a4e6
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-12 14:51:34 +1100

    Routes index created

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Header from "./components/header"
+import Footer from './components/footer';
+import TitleHeader from './components/titleHeader';
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <Header/>
+      <TitleHeader/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  "./style.css"
+
+const Footer = (props) => {
+  return (
+    <footer id="sticky-footer " class=" py-4  " color="light" >
+    <div class="container text-center">
+      <small>© Copyright - Antonio Guedes </small>
+    </div>
+  </footer>
+  );
+};
+
+export default Footer;
