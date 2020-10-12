63b8df16eb9dc0bc7cb6f6108f3f1504d747e0c5
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-12 15:54:57 +1100

    Typo fixed at client index

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Header from "./components/header"
 import Footer from './components/footer';
