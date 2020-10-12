f3533adb63191a68ca1dbf14e8e084c2ef1a5c40
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-12 12:12:24 +1100

    bookContainer created

--- google-books-search/src/App.js
+++ google-books-search/src/App.js
@@ -20,7 +20,6 @@ function App() {
           <Route exact path="/saved" component={Saved} />
           <Route component={NoMatch} />
         </Switch>
-      <Footer/>
       </div>
     </Router>
   );
--- google-books-search/src/pages/Home.js
+++ google-books-search/src/pages/Home.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import SearchBar from '../components/searchBar';
 import { Jumbotron, Button } from 'reactstrap';
+import BooksContainer from '../components/booksContainer';
 
 
 const Home = (props) => {
   return (
       <div>
       <SearchBar/>
-      <Jumbotron className="container-fluid  m-5 clearfix" style={{ padding: '.5rem',backgroundColor: "#F8F9FA" }}>
-        <h1 className="d-flex justify-content-center display-4">(REACT) Google Books Search</h1>
-        <p className="d-flex justify-content-center lead">Search and Save Books of Interest</p>
-        </Jumbotron>
+      <BooksContainer/>
       </div>
   );
 };
